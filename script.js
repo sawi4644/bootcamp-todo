f460cf490b95bba78ab0d6ed9256f8f34165324e
Sara Wilson <sarakatewilson98@gmail.com>
2020-11-21 13:37:25 -0600

    adding new todo item

--- script.js
+++ script.js
@@ -5,15 +5,36 @@ var todoCountSpan = document.querySelector("#todo-count");
 
 var todos = ["Learn HTML", "Learn CSS", "Learn JavaScript"];
 function renderTodos(){
+    // clear exisiting to dos
+    todoList.innerHTML = ''
+    // re render todos
     for(var i = 0; i< todos.length; i ++){
     // create li element
     var li= document.createElement('li')
     // add todo text to li
     todos[i]
+    // add text to li
+    li.innerText = todos [i]
     // append list item into to do ul
     todoList.appendChild(li)
     
     }
 }
+function addTodo(event){
+    event.preventDefault()
+    // get todoInput value
+    var newTodo = todoInput.value
+
+    if(newTodo === ''){
+        return;
+    }
+    // push value into array of todos
+    todos.push(newTodo)
+    todoInput.value = null
+    renderTodos()
+
+}
+
+todoForm.addEventListener('submit', addTodo)
 // calling function so itll run
 renderTodos()
